8851b63a31c65166612717f0f3d7a23be09b5e21
Anaïs Ramírez <90638339+AnaisRamirez-SagoMini@users.noreply.github.com>
2024-08-07 12:00:45 -0400

    Fix CameraController.cs formatting

--- CameraController.cs
+++ CameraController.cs
@@ -1,17 +1,14 @@
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class CameraController : MonoBehaviour {
+	// Start is called before the first frame update
+	void Start() {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+	}
     
 }
