ef28162fa94baf42b202d52d5be92a3f834e0b71
Anaïs Ramírez <90638339+AnaisRamirez-SagoMini@users.noreply.github.com>
2024-08-19 13:20:15 -0400

    follow jinja transform

--- CameraController.cs
+++ CameraController.cs
@@ -1,7 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour {
-	
+	// a field to store jinja's transform
+	[SerializeField]
+	Transform jinjaTransform = null;
+
 	// Start is called before the first frame update
 	void Start() {
 
@@ -9,7 +14,7 @@ public class CameraController : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update() {
-
+		// move the camera to jinja's position
+		GetComponent<Transform>().position = jinjaTransform.position;
 	}
-    
 }
