edf32ff01dae092d0f167d00aabc00d36be6286a
Anaïs Ramírez <90638339+AnaisRamirez-SagoMini@users.noreply.github.com>
2024-08-19 10:57:57 -0400

    Move with right arrow

--- JinjaController.cs
+++ JinjaController.cs
@@ -1,7 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class JinjaController : MonoBehaviour {
-
 	// Start is called before the first frame update
 	void Start() {
 
@@ -9,7 +10,10 @@ public class JinjaController : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update() {
-
+		// check if the right arrow is down
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			// move jinja to the right
+			GetComponent<Transform>().position += Vector3.right;
+		}
 	}
-
 }
