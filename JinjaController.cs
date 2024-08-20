5286befe72bf2fbe8d05aa7bf61fd6e95a1b98aa
Anaïs Ramírez <90638339+AnaisRamirez-SagoMini@users.noreply.github.com>
2024-08-20 10:18:27 -0400

    add collision raycast

--- JinjaController.cs
+++ JinjaController.cs
@@ -2,8 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class JinjaController : MonoBehaviour
-{
+public class JinjaController : MonoBehaviour {
 
 	// a field to store Jinja's velocity
 	Vector3 velocity = Vector3.zero;
@@ -21,14 +20,12 @@ public class JinjaController : MonoBehaviour
 	float gravity = 0.02f;
 
 	// Start is called before the first frame update
-	void Start()
-	{
+	void Start() {
 
 	}
 
 	// Update is called once per frame
-	void Update()
-	{
+	void Update() {
 
 		// variable for jinja's transform component
 		Transform tf = GetComponent<Transform>();
@@ -40,20 +37,41 @@ public class JinjaController : MonoBehaviour
 		velocity += Vector3.down * gravity;
 
 		// check if the right arrow is down
-		if (Input.GetKey(KeyCode.RightArrow))
-		{
+		if (Input.GetKey(KeyCode.RightArrow)) {
 			// increasing velocity
 			velocity += Vector3.right * acceleration;
 		}
 
 		// check if the left arrow is down
-		if (Input.GetKey(KeyCode.LeftArrow))
-		{
+		if (Input.GetKey(KeyCode.LeftArrow)) {
 			// decreasing velocity
 			velocity += Vector3.left * acceleration;
 		}
 
 		//move jinja by her current velocity
 		tf.position += velocity;
+
+		//COLLISION
+
+		//the start of the ray (jinja's position before she moved)
+		Vector3 origin = tf.position - velocity;
+
+		// the direction of the ray
+		Vector3 direction = velocity;
+
+		// the length of the ray (the distance jinja moved)
+		float length = velocity.magnitude;
+
+		// the result of the raycast
+		RaycastHit2D hit = Physics2D.Raycast(origin, direction, length);
+
+		// did jinja go down through a platform when she moved?
+		if (velocity.y < 0 && hit.collider != null) {
+			// put jinja exactly on the platform
+			tf.position = new Vector3(tf.position.x, hit.point.y, tf.position.z);
+
+			// reset her y velocity
+			velocity.y = 0;
+		}
 	}
 }
