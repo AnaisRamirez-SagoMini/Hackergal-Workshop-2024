e56ca9ef11454ff538faf38d88239f6c659302c5
Anaïs Ramírez <90638339+AnaisRamirez-SagoMini@users.noreply.github.com>
2024-08-20 09:54:38 -0400

    add gravity

--- JinjaController.cs
+++ JinjaController.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class JinjaController : MonoBehaviour {
+public class JinjaController : MonoBehaviour
+{
 
 	// a field to store Jinja's velocity
 	Vector3 velocity = Vector3.zero;
@@ -15,13 +16,19 @@ public class JinjaController : MonoBehaviour {
 	[SerializeField]
 	float friction = 0.9f;
 
+	// a field to store jinja's gravity setting
+	[SerializeField]
+	float gravity = 0.02f;
+
 	// Start is called before the first frame update
-	void Start() {
+	void Start()
+	{
 
 	}
 
 	// Update is called once per frame
-	void Update() {
+	void Update()
+	{
 
 		// variable for jinja's transform component
 		Transform tf = GetComponent<Transform>();
@@ -29,14 +36,19 @@ public class JinjaController : MonoBehaviour {
 		// add friction
 		velocity.x *= friction;
 
+		//apply gravity 
+		velocity += Vector3.down * gravity;
+
 		// check if the right arrow is down
-		if (Input.GetKey(KeyCode.RightArrow)) {
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
 			// increasing velocity
 			velocity += Vector3.right * acceleration;
 		}
 
 		// check if the left arrow is down
-		if (Input.GetKey(KeyCode.LeftArrow)) {
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
 			// decreasing velocity
 			velocity += Vector3.left * acceleration;
 		}
