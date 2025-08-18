5f792a1352d0de937ddb1cfb6004c4afa29d7e77
Anaïs Ramírez <90638339+AnaisRamirez-SagoMini@users.noreply.github.com>
2025-08-18 10:53:11 -0400

    Update JinjaController.cs

--- JinjaController.cs
+++ JinjaController.cs
@@ -4,5 +4,12 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class JinjaController : MonoBehaviour {
+		// get jinja's transform component
+		Transform tf = GetComponent<Transform>();
 
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			// move jinja one unit to the right
+			tf.position += Vector3.right;
+		}
 }
