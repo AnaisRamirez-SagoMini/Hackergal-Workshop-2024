bbd81459542813b999c6bec74c78219a672088d6
Anaïs Ramírez <90638339+AnaisRamirez-SagoMini@users.noreply.github.com>
2025-08-18 13:20:43 -0400

    Update JinjaController.cs

--- JinjaController.cs
+++ JinjaController.cs
@@ -15,6 +15,10 @@ public class JinjaController : MonoBehaviour
 	[SerializeField]
 	float friction = 0.9f;
 
+	// a field to store jinja's gravity setting
+	[SerializeField]
+	float gravity = 0.02f;
+
 	// Start is called before the first frame update
 	void Start()
 	{
@@ -41,6 +45,9 @@ public class JinjaController : MonoBehaviour
 
 		// apply friction
 		velocity.x *= friction;
+
+		// apply gravity
+		velocity += Vector3.down * gravity;
 
 		// move jinja by her current velocity
 		tf.position += velocity;
